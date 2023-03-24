4505460520342dfe2d4213311a7527ad0e8b9a6e
X4SS4 <xas.zab.kam@gmail.com>
2023-03-24 22:34:42 +0400

    Main page
    2 buttons + Design

---
+++ WPF project N 000047/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_project_N_000047
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Sign_in_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Sign_in.Foreground = Brushes.Black;
+        }
+
+        private void Sign_up_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Sign_up.Foreground = Brushes.Black;
+        }
+
+        private void Sign_in_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Sign_in.Foreground = Brushes.Yellow;
+        }
+
+        private void Sign_up_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Sign_up.Foreground = Brushes.Yellow;
+        }
+    }
+}
